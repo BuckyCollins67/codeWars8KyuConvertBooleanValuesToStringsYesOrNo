7d837fde7b2c587b2d90142eea650d1fda8f775b
BuckyCollins67 <bucky.collins.67@gmail.com>
2022-10-11 20:26:18 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function boolToWord( bool ){
+    if (bool === true){
+      return('Yes')
+    }else{
+      return('No')
+    }
+  }
+  
+  //PREP
+  
+  //  P - Parameters
+  //  Boolean true, or Boolean false
+  
+  //  R - Returns
+  //  String "Yes" for true, String "No" for false
+  
+  //  E - Examples
+  //  boolToWord(true) -> return 'yes'
+  //  boolToWord(false) -> return 'no'
+  
+  //  P - Pseudocode
+  //  If I get true, return 'yes', otherwise, return 'no'
